a4ac948a84a5399d52f8ccb502f9f0e9f03dec9a
Mark Ryall <mark@ryall.name>
2012-01-24 10:13:15 +1000

    added a tmp folder for test folders and removed all actual file operations (now outputs a script)

--- lib/folder_synchroniser.rb
+++ lib/folder_synchroniser.rb
@@ -3,19 +3,17 @@ require 'historical_traverser'
 require 'change_resolver'
 require 'FileUtils'
 
-include FileUtils
-
 class FolderSynchroniser
   IGNORE_PATTERNS = [
     /\.DS_Store$/
   ]
 
   def initialize left, right, previous
-    @previous = previous
+    @previous = previous || 'history'
     @left = Traverser.new Pathname.new(left)
     @right = Traverser.new Pathname.new(right)
-    @old = HistoricalTraverser.new previous
-    @new = File.open(previous+'.tmp', 'w')
+    @old = HistoricalTraverser.new @previous
+    @new = File.open(@previous+'.tmp', 'w')
     @resolver = ChangeResolver.new @left, @right, @old
   end
 
@@ -25,7 +23,7 @@ class FolderSynchroniser
   end
 
   def both_modified
-    puts "Both of the following have changed - please resolve manually:\n\t#{left.current}\n\t#{right.current}"
+    puts "# Both of the following have changed - please resolve manually:\n\t#{left.current}\n\t#{right.current}"
     report old
     progress_all
   end
@@ -43,21 +41,13 @@ class FolderSynchroniser
   end
 
   def left_modified
-    if checksums_differ?
-      copy_left_to_right
-    else
-      touch left.current, right.current
-    end
+    copy_left_to_right
     report left
     progress_all
   end
 
   def right_modified
-    if checksums_differ?
-      copy_right_to_left
-    else
-      touch right.current, left.current
-    end
+    copy_right_to_left
     report right
     progress_all
   end
@@ -90,9 +80,7 @@ private
   attr_reader :left, :right, :old
 
   def debug message
-    if ENV['DEBUG']
-      puts message
-    end
+    puts message if ENV['DEBUG']
   end
 
   def step
@@ -107,29 +95,27 @@ private
 
   def close
     @new.close
-    mv @previous+'.tmp', @previous 
+    `mv #{@previous}.tmp #{@previous}`
   end
 
-  def confirm message
-    puts message
-    if ENV['SKIP_CONFIRMATION'] 
-      yield
-    else
-      print "? "
-      if $stdin.gets.chomp.downcase == 'no'
-        puts 'skipped'
-      else
-        yield
-      end
-    end
+  def rm path
+    puts "rm \"#{path}\""
+  end
+
+  def mkdir
+    puts "mkdir -p \"#{path}\""
+  end
+
+  def cp from, to
+    puts "cp -p \"#{from}\" \"#{to}\""
   end
 
   def remove_right
-    confirm("Delete:\n\t#{right.current}") { rm right.current.to_s }
+    rm right.current
   end
 
   def remove_left
-    confirm("Delete:\n\t#{left.current}") { rm left.current.to_s }
+    rm left.current
   end
 
   def copy_right_to_left
@@ -144,44 +130,18 @@ private
     source_path, destination_path = source.current.to_s, target.base.to_s+'/'+source.relative
     IGNORE_PATTERNS.each do |pattern|
       if pattern.match(source_path)
-        puts "Skipping copying #{source_path}"
+        rm source_path
         return
       end
     end
     destination_directory = File.dirname(destination_path)
-    unless File.exist? destination_directory
-      confirm("Create directory:\n\t#{File.dirname(destination_path)}") { mkdir_p File.dirname(destination_path) }
-    end
-    confirm "Copy:\n\t#{source_path}\nTo\n\t#{destination_path}" do
-      cp source_path, destination_path
-      touch source_path, destination_path
-    end
-  end
-
-  def bash_escape string
-    string = string.to_s
-    " `'$&(){};".scan(/./).each {|char| string = string.split(char).join('\\'+char) }
-    string
-  end
-
-  def checksum path
-    path = bash_escape path
-    `md5 -q #{path}`.chomp.tap {|cs| puts "Checksum for\n\t#{path}\n\t#{cs}"}
-  end
-
-  def touch source, target
-    source, target = bash_escape(source), bash_escape(target)
-    run "touch -r #{source} #{target}"
-    run "touch -r #{target} #{source}"
+    mkdir destination_directory unless File.exist? destination_directory
+    cp source_path, destination_path
   end
 
   def run command
     debug command
     system command
-  end
-
-  def checksums_differ?
-    checksum(left.current) != checksum(right.current)
   end
 
   def progress_left
