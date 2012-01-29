700698d44a72a778b336a7cd8e0d0ec44907152a
Mark Ryall <mark@ryall.name>
2012-01-29 14:16:29 +1000

    moved contents of drain into dir sync module and changed it to process multiple scripts

--- lib/dir_sync.rb
+++ lib/dir_sync.rb
@@ -17,4 +17,24 @@ module DirSync
     loop { break unless resolver.iterate }
     history.close
   end
+
+  def self.drain *paths
+    if paths.empty?
+      puts "usage: #{__FILE__} *scripts"
+      puts "  Runs a set of single line commands one by one (so that you can interrupt and the script will resume)"
+      exit 1
+    end
+
+    paths.each do |path|
+      lines = File.readlines(path).map {|s| s.chomp.strip }.select {|s| !s.empty? and !s.start_with?('#')}
+
+      loop do
+        break if lines.empty?
+        lines.shift.tap {|s| puts "> #{s}" }.tap {|s| puts `#{s}` }
+        File.open("#{path}.tmp", 'w') {|f| f.puts lines.join "\n" }
+        `mv #{path}.tmp #{path}`
+      end
+      `rm #{path}`
+    end
+  end
 end
